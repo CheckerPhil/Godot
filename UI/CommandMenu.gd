98914653f2a06e566c7ec16ad051f2f7ebc26d89
CheckerPhil <91223795+CheckerPhil@users.noreply.github.com>
2021-12-31 23:26:02 +0100

    Update CommandMenu.gd

--- UI/CommandMenu.gd
+++ UI/CommandMenu.gd
@@ -20,7 +20,11 @@ func _on_Input_text_entered(new_text):
 		output_text(text)
 	elif new_text == prefix + "noai":
 		Settings.ai = false
+		var text = "AI turned off"
+		output_text(text)
 	elif new_text == prefix + "quit":
+		var text = "Quitting using Console..."
+		output_text(text)
 		get_tree().quit(99)
 	elif new_text == "Soos ist ein geiler Minecraft Server":
 		OS.shell_open("https://soos-network.github.io/Soos-Website/")
